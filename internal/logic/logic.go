17cef305563ee992a3d28b084241a74f00a94144
Ali Talebi <sudofrost@gmail.com>
2024-09-01 23:18:58 +0330

    Add `NewID` function for uique ID generation

--- internal/logic/logic.go
+++ internal/logic/logic.go
@@ -6,6 +6,16 @@ import (
 
 var tracker *models.Tracker
 
+func NewID() float64 {
+	var max float64
+	for _, expense := range tracker.Expenses {
+		if expense.ID > max {
+			max = expense.ID
+		}
+	}
+	return max + 1
+}
+
 func init() {
 	tracker = models.NewTracker()
 }
