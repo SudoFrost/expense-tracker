e2382066f0c402a418fd9b94e5073c5b667e2ea2
Ali Talebi <sudofrost@gmail.com>
2024-09-02 22:30:52 +0330

    Integrate `storage` package for expense persistence

--- internal/logic/logic.go
+++ internal/logic/logic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"github.com/sudofrost/expense-tracker/internal/models"
+	"github.com/sudofrost/expense-tracker/internal/storage"
 )
 
 var tracker *models.Tracker
@@ -24,4 +25,20 @@ func AddExpense(description string, amount float64) float64 {
 
 func init() {
 	tracker = models.NewTracker()
+
+	expenses, err := storage.Load()
+	if err != nil {
+		panic(err)
+	}
+	for _, expense := range expenses {
+		tracker.Expenses = append(tracker.Expenses, &expense)
+	}
+}
+
+func Save() error {
+	expenses := make([]models.Expense, 0, len(tracker.Expenses))
+	for _, expense := range tracker.Expenses {
+		expenses = append(expenses, *expense)
+	}
+	return storage.Save(expenses)
 }
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/sudofrost/expense-tracker/cmd"
+import (
+	"github.com/sudofrost/expense-tracker/cmd"
+	"github.com/sudofrost/expense-tracker/internal/logic"
+)
 
 func main() {
 	cmd.Execute()
+	defer logic.Save()
 }
