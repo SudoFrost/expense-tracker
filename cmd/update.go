52e5fe961031d94583a3d109b438d38859b58dc8
Ali Talebi <sudofrost@gmail.com>
2024-09-03 03:05:26 +0330

    Add `update` command to update an expense

---
+++ cmd/update.go
@@ -0,0 +1,64 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/spf13/cobra"
+	"github.com/sudofrost/expense-tracker/internal/logic"
+)
+
+// updateCmd represents the update command
+var updateCmd = &cobra.Command{
+	Use:   "update [id]",
+	Short: "Update an expense",
+	Long:  `Update an expense`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Fprintln(os.Stderr, "ID is required")
+			return
+		}
+		id, err := strconv.ParseFloat(args[0], 64)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Invalid ID")
+			return
+		}
+
+		expense, err := logic.GetExpense(id)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			return
+		}
+
+		var description = expense.Description
+		var amount = expense.Amount
+
+		if cmd.Flags().Changed("description") {
+			description, _ = cmd.Flags().GetString("description")
+		}
+
+		if cmd.Flags().Changed("amount") {
+			amount, _ = cmd.Flags().GetFloat64("amount")
+		}
+
+		err = logic.UpdateExpense(id, description, amount)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			return
+		}
+
+		fmt.Printf("Expense updated successfully. (ID: %v)\n", id)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(updateCmd)
+	updateCmd.Flags().StringP("description", "d", "", "Description of the expense")
+	updateCmd.Flags().Float64P("amount", "a", 0, "Amount of the expense")
+
+	updateCmd.MarkFlagsOneRequired("description", "amount")
+}
